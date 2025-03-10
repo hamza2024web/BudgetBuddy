f24b7fa01bdc1a561f6e75b567f47eb77ca728c4
hamza2024web <hamzaakroubi1528@gmail.com>
2025-03-10 14:18:38 +0000

    add a migration of tags and add the tag model

--- project/app/Models/Tag.php
+++ project/app/Models/Tag.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Tag extends Model
 {
-    use HasFactory;
+    use HasFactory , HasApiTokens , Notifiable;
+    protected $fillable = [
+        'name',
+    ];
 }
